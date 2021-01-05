7817b4530534d5d0e6a803182d3dc716af218f76
edianibarrola <edianibarrola@gmail.com>
2021-01-05 18:16:26 +0000

    update

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,25 +1,29 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			userInput: "",
+			inputArray: [],
+			url: "https://assets.breatheco.de/apis/fake/todos/user/edian"
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
+			loadInitialData: () => {
+				{
+					fetch(getStore().url)
+						.then(function(response) {
+							if (!response.ok) {
+								throw Error(response.statusText);
+							}
+							return response.json();
+						})
+						.then(jsonifiedResponse => setStore({ inputArray: jsonifiedResponse }))
+						.catch(function(error) {
+							console.log("Looks like there was a problem: \n", error);
+						});
+				}
 				/**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
@@ -37,9 +41,53 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+			removeListItem(id) {
+				setStore({
+					inputArray: getStore().inputArray.filter((input, index) => index != id)
+				});
+			},
+
+			addToListButton(e) {
+				let userInput = getStore().userInput;
+				if (userInput) {
+					let newItem = { label: userInput, done: false };
+					var newInputArray = getStore().inputArray.concat(newItem);
+					setStore({
+						inputArray: newInputArray,
+						userInput: ""
+					});
+				}
+				fetch(getStore().url, {
+					method: "PUT", // or 'POST'
+					body: JSON.stringify(getStore().inputArray), // data can be `string` or {object}!
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(res => res.json())
+					.then(response => console.log("Success:", response))
+					.catch(error => console.error("Error:", error));
+			},
+			addToListEnter(e) {
+				const store = getStore();
+				let userInput = getStore().userInput;
+
+				if (e.keyCode == 13 && userInput) {
+					let newItem = { label: userInput, done: false };
+					let newInputArray = store.inputArray.concat(newItem);
+					setStore({
+						inputArray: newInputArray,
+						userInput: ""
+					});
+				}
+			},
+			setUserInput(event) {
+				setStore({
+					userInput: getStore().userInput + event.target.value
+				});
 			}
 		}
 	};
 };
-
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,44 @@
-import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	const [userInput, setUserInput] = useState("");
+	return (
+		<div className="container">
+			<div className="text-center mt-5">
+				<h1>{"ADDlist "} </h1>
+				<div className="input-group">
+					<input
+						type="text"
+						className="form-control"
+						aria-label="Large"
+						aria-describedby="inputGroup-sizing-sm"
+						value={userInput}
+						onChange={actions.setUserInput}
+						//onChange={e => setUserInput(e.target.value)}
+						onKeyUp={() => actions.addToListEnter(userInput)}
+					/>
+					<button className="input-group-append addButton" onClick={() => actions.addToListButton(userInput)}>
+						ADD it up!
+					</button>
+				</div>
+			</div>
+			<div>
+				<ol className="col-4 mx-auto light ">
+					{store.inputArray.map((listItem, index) => {
+						return (
+							<li key={index}>
+								{listItem.label}{" "}
+								<input className="float-left d-flex-inline" type="checkbox" key={index} />
+								<i className="fas float-right fa-ban" onClick={() => actions.removeListItem(index)} />
+							</li>
+						);
+					})}
+				</ol>
+			</div>
+		</div>
+	);
+};
